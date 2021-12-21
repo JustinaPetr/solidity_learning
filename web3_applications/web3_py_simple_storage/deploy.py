1fc134e0e07757909bf93082be554a55c71f6601
JustinaPetr <juste@rasa.com>
2021-12-21 19:20:55 +0200

    add functions to interact with the contract

--- web3_applications/web3_py_simple_storage/deploy.py
+++ web3_applications/web3_py_simple_storage/deploy.py
@@ -1,6 +1,7 @@
 from solcx import compile_standard, install_solc
 from web3 import Web3
 import json
+import os
 
 
 with open("./SimpleStorage.sol", "r") as file:
@@ -36,9 +37,9 @@ abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
 # details for connecting to ganache
 w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:8545"))
-chain_id = 5777
+chain_id = 1337
 address = "0x7AafCa0FeaDb0d3a17c25eA4933d99660a2C567e"
-private_key = "0x4dfc99c8575562712a44ec4592592435e3d82078686a686d842e61bc191f6f90"
+
 
 # create a contract with python
 SimpleStorage = w3.eth.contract(abi = abi, bytecode = bytecode)
@@ -46,4 +47,23 @@ SimpleStorage = w3.eth.contract(abi = abi, bytecode = bytecode)
 # get the latest transaction
 nonce = w3.eth.getTransactionCount(address)
 
-transaction = SimpleStorage.constructor().buildTransaction({"chainId": chain_id, "from": my_address, "nonce": nonce})
+transaction = SimpleStorage.constructor().buildTransaction({"gasPrice": w3.eth.gas_price, "chainId": chain_id, "from": address, "nonce": nonce,})
+
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key = os.getenv("PRIVATE_KEY"))
+
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+
+# working with the contract
+
+simple_storage = w3.eth.contract(address = tx_receipt.contractAddress, abi = abi)
+
+# calling a view function
+print(simple_storage.functions.retrieve().call())
+
+store_transaction = simple_storage.functions.store(15).buildTransaction({"gasPrice": w3.eth.gas_price, "chainId": chain_id, "from": address, "nonce": nonce + 1,})
+signed_store_txn = w3.eth.account.sign_transaction(store_transaction, private_key = os.getenv("PRIVATE_KEY"))
+tx_store_hash = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+tx_store_receipt = w3.eth.wait_for_transaction_receipt(tx_store_hash)
